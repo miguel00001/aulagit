60a1fea5323c647cb8215cfb1ba0810e297119ea
miguel00001 <miguelkarlos867@gmail.com>
2024-03-27 21:10:32 -0300

    adicione seu nome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+         String nome=JOptionPane.showInputDialog(null,"digite seu nome" );
+        JOptionPane.showInputDialog(null,"digite seu numero" );
 
     }
 }
